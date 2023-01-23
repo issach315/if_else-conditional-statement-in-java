ddaac81a87744526bd9a47f030f3138728eca9a5
issach315 <issachemani227@gmial.com>
2023-01-23 10:14:38 +0530

    conditional statement in java if-else

---
+++ if_else.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class JavaDay4
+{
+public static void main(String[] args)
+ {Scanner sc = new Scanner(System.in);
+    System.out.println("enter your Age for voting ");
+     int age = sc.nextInt();
+
+     if (age>=18)
+      {
+
+        System.out.println("your eligible for voting ");
+        
+     }
+      else
+       {
+            System.out.println("your not eligiblr for voting your age is below 18 years");        
+     }
+           
+}
+}
